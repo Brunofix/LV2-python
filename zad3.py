8bc36e051d5a9efef343caa081375c07a8670091
Bruno Šarlija <bruno.sarlija@gmail.com>
2023-04-03 13:35:01 +0200

    comit

--- zad3.py
+++ zad3.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy import ndarray
 
 img = plt.imread("tiger.png")
 img = img[:,:,0].copy()
@@ -11,27 +12,29 @@ plt.figure()
 plt.imshow(img, cmap="gray")
 plt.title("Original image")
 
+brighter_img = np.clip(img * 2, 0, 1)
+plt.figure()
+plt.imshow(brighter_img, cmap="gray")
+plt.title("svjetlija")
 
 
-rotated_img = np.zeros((w, h))
+rotirana_img: ndarray = np.zeros((w, h))
 for y in range(h):
     for x in range(w):
         rotated_y = x
         rotated_x = h - 1 - y
-        rotated_img[rotated_y, rotated_x] = img[y, x]
-
-mirrored_img = np.zeros((w, h))
-for y in range(w):
-    for x in range(h):
-        mirrored_x = h - 1 - x
-        mirrored_img[y, mirrored_x] = rotated_img[y, x]
-
-
-
-
-print(img.shape)
-print(img.dtype)
+        rotirana_img[rotated_y, rotated_x] = img[y, x]
 plt.figure()
-plt.imshow(img, cmap="gray")
+plt.imshow(rotirana_img, cmap="gray")
+plt.title("rotirana")
+
+preslikana_slika = np.zeros((h,w))
+for x in range(h):
+    mirrored_x = h - 1 - x
+    preslikana_slika[mirrored_x,:] = img[x,:]
+plt.figure()
+plt.imshow(preslikana_slika, cmap="gray")
+plt.title("preslikanal")
+
 
 plt.show()
